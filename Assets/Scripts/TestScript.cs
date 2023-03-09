c2967a0b0c1c9e9b1af2837c03be7b61c5bc003d
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-03-09 09:15:48 +0900

    コンポーネントの操作を試した

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestScript : MonoBehaviour
 {
     public GameObject TestObject;
     private GameObject sphereObj;
     private GameObject sphereObjCopied;
+    private GameObject box;
     public BoxCollider A;
+    public TestTest Z;
+    public Text Text;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,7 @@ public class TestScript : MonoBehaviour
         //TestObject.transform.localEulerAngles = new Vector3(1.0f, 10.0f, 10.0f);
         TestObject.transform.localScale = new Vector3(1.0f, 10.0f, 10.0f);
         TestObject.SetActive(true);
+        A.size = new Vector3(10.0f, 10.0f, 10.0f);
         Debug.Log(TestObject.activeSelf);
         Debug.Log(TestObject.activeInHierarchy);
         Debug.Log(TestObject != null);
@@ -27,7 +32,22 @@ public class TestScript : MonoBehaviour
         sphereObjCopied = Instantiate(sphereObj, new Vector3(1.5f, 0.0f, 0.0f), Quaternion.identity);
         sphereObjCopied.name = "Sphere2";
 
+        /*
+         *  コンポーネント（機能）の操作
+         */
+        Z.B = "変更しました";
+        Z.CCC();
 
+        box = GameObject.Find("CubeA");
+        BoxCollider b = box.GetComponent<BoxCollider>();
+        b.size = new Vector3(3.0f, 3.0f, 3.0f);
+
+        Rigidbody rb = box.AddComponent<Rigidbody>();
+        rb.mass = 5.0f;
+
+        Destroy(rb, 2.0f); // コンポーネント削除
+
+        Text.text = "AB\nCD\nEF";
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/TestTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTest : MonoBehaviour
+{
+    public string B;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CCC()
+    {
+        Debug.Log(B);
+    }
+}
